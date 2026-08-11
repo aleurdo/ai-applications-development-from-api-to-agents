f4180f136f6b321c00fff85f1e4125cc6a957205
aleurdo <7777846@gmail.com>
2026-08-11 15:22:57 +0400

    Add validation for api_key in base.py

--- t1_llm_api/openai/base.py
+++ t1_llm_api/openai/base.py
@@ -29,6 +29,13 @@ class BaseOpenAIClient(AIClient, ABC):
         """
         #TODO:
         # Make validation for `api_key` (the same as in parent class)
+        if not api_key or api_key.strip() == "": 
+            raise ValueError("API key cannot be null or empty")
         # Call to __init__ of super class
+        super().__init__(
+            endpoint=endpoint, 
+            model_name=model_name, 
+            api_key=f"Bearer {api_key}", 
+            system_prompt=system_prompt, )
         # The `api_key` should be formatted according to Open AI specification
         raise NotImplementedError
